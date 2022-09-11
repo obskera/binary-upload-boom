df33e3866549ae79593e939e1fa73ed2dc965cc3
obskera <vivifydecepi@gmail.com>
2022-09-10 20:10:09 -0700

    updates

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB_STRING, {
+      // const conn = await mongoose.connect('mongodb://localhost:27017/BUB', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
